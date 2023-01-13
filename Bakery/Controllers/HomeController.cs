950685225ffc6758a0ae536658d568e73af3bfd7
Anton Ch <43277455+anton3ch@users.noreply.github.com>
2023-01-13 12:49:19 -0600

    add controllers

---
+++ Bakery/Controllers/HomeController.cs
@@ -0,0 +1,73 @@
+using Microsoft.AspNetCore.Mvc;
+using Bakery.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace Bakery.Controllers
+{
+    public class HomeController : Controller
+    {
+      private readonly BakeryContext _db;
+      private readonly UserManager<ApplicationUser> _userManager;
+
+      public HomeController(UserManager<ApplicationUser> userManager, BakeryContext db)
+      {
+        _userManager = userManager;
+        _db = db;
+      }
+
+      [HttpGet("/")]
+      public async Task<ActionResult> Index()
+      {
+        // Flavor logic
+        Flavor[] flavors = _db.Flavors.ToArray();
+        Dictionary<string,object[]> model = new Dictionary<string, object[]>();
+        model.Add("flavors", flavors);
+        // Treat logic
+        string userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);
+        if (currentUser != null)
+        {
+          Treat[] treats = _db.Treats
+                      .Where(entry => entry.User.Id == currentUser.Id)
+                      .ToArray();
+          model.Add("treats", treats);
+        }
+        return View(model);
+      }
+
+      public ActionResult Search(string query)
+      {
+        Dictionary<string,object[]> model = new Dictionary<string, object[]>();
+        Treat[] treats = _db.Treats.Where(treat => treat.Name.Contains(query)).ToArray();
+        model.Add("treats", treats);
+        Flavor[] flavors = _db.Flavors.Where(flavor => flavor.Name.Contains(query)).ToArray();
+        model.Add("flavors", flavors);
+        return View(model);
+      }
+    }
+}
+
+// [HttpGet("/")]
+//       public async Task<ActionResult> Index()
+//       {
+//         // Category logic
+//         Category[] cats = _db.Categories.ToArray();
+//         Dictionary<string,object[]> model = new Dictionary<string, object[]>();
+//         model.Add("categories", cats);
+//         // Item logic
+//         string userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+//         ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);
+//         if (currentUser != null)
+//         {
+//           Item[] items = _db.Items
+//                       .Where(entry => entry.User.Id == currentUser.Id)
+//                       .ToArray();
+//           model.Add("items", items);
+//         }
+//         return View(model);
+//       }
+//     }
